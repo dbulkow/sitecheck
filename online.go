5cdc9addd54cc6a5be0e9984c6496a9974e2c02e
David Bulkow <dbulkow@gmail.com>
2015-08-22 16:44:47 -0400

    add a status web template

--- online.go
+++ online.go
@@ -1,14 +1,42 @@
 package main
 
 import (
-	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"text/template"
 )
 
 func statusHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	t, err := template.ParseFiles("status.html")
+	if err != nil {
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	status := []struct {
+		Name    string
+		Service string
+		Status  string
+	}{
+		{
+			Name:    "name1",
+			Service: "service1",
+			Status:  "status1",
+		},
+		{
+			Name:    "name2",
+			Service: "service2",
+			Status:  "status2",
+		},
+	}
+
+	err = t.Execute(w, status)
+	if err != nil {
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
 }
 
 func main() {
