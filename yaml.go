ef495a6c0e6da51f67fa824a92b349c606f6a665
David Bulkow <dbulkow@gmail.com>
2016-05-29 11:14:57 -0400

    demonstrate d3 display

--- yaml.go
+++ yaml.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
+	"log"
+	"net/http"
 
 	"gopkg.in/yaml.v2"
 )
@@ -14,26 +16,64 @@ type Config struct {
 	URL     []string `yaml:"url"`
 }
 
+type Sites struct {
+	cfg      []*Config
+	TopLevel string  `json:"name"`
+	Sites    []*Site `json:"children"`
+}
+
+type URL struct {
+	Name string `json:"name"`
+	Size int    `json:"size"`
+}
+
+type Site struct {
+	Name string `json:"name"`
+	URLs []*URL `json:"children"`
+}
+
+func (s *Sites) statusHandler(w http.ResponseWriter, r *http.Request) {
+	b, err := json.MarshalIndent(s, "", "    ")
+	if err != nil {
+		http.Error(w, "Internal Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.Write(b)
+}
+
 func main() {
 	data, err := ioutil.ReadFile("sitecheck.yml")
 	if err != nil {
 		panic(err)
 	}
 
-	var cfg []Config
+	var sites Sites
 
-	err = yaml.Unmarshal(data, &cfg)
+	err = yaml.Unmarshal(data, &sites.cfg)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, c := range cfg {
-		fmt.Println("name:", c.Name)
-		fmt.Println("type:", c.Type)
-		fmt.Println("url:")
+	sites.TopLevel = "sites"
+	sites.Sites = make([]*Site, 0)
+	for _, c := range sites.cfg {
+		urls := make([]*URL, 0)
 		for _, u := range c.URL {
-			fmt.Printf("  %s\n", u)
+			urls = append(urls, &URL{Name: u, Size: 10})
 		}
-		fmt.Println()
+
+		s := &Site{
+			Name: c.Name,
+			URLs: urls,
+		}
+
+		sites.Sites = append(sites.Sites, s)
 	}
+
+	http.HandleFunc("/sitecheck.json", sites.statusHandler)
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir(""))))
+
+	log.Fatal(http.ListenAndServe(":9999", nil))
 }
